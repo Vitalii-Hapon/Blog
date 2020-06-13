4ddb7da75167c39c4cd9ca8afaab7b9a35dc7a03
Vitalii-Hapon <vitali_gapon@gmail.com>
2020-06-13 17:13:04 +0300

    ADD home page, post page, main-layouts, roots; UPDATE 2020.06.13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PostPageComponent } from './pages/post-page/post-page.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PostPageComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
